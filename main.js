8f62e7c0506ccf00219671a9e598387999aafb1a
Shaqqour <mshaqqourm@gmail.com>
2020-03-14 17:47:52 -0700

    Done.

--- main.js
+++ main.js
@@ -2,9 +2,40 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+let glyphStates = {
+  "♡": "♥",
+  "♥": "♡"
+};
+
+let colorStates = {
+  "red": "",
+  "": "red"
+};
+
 // Your JavaScript code goes here!
 
-
+document.addEventListener("DOMContentLoaded", () => {
+  let likeHearts = document.getElementsByTagName("li");
+  for(let i = 0; i < likeHearts.length; i++) {
+    likeHearts[i].addEventListener("click", function (e) {
+      let heart = e.target;
+      mimicServerCall()
+        .then(function (serverMessage) {
+          // STEP 2: Uncomment the next 3 lines.
+          //We'll use Pillar 1 (DOM Manipulation) to update the screen and
+          //mimic Pillar 3 (Server Communication) to only update the screen if the
+          //sending of information to the server succeeds.
+          alert("You notified the server!");
+          alert(serverMessage);
+          heart.innerText = glyphStates[heart.innerText];
+          heart.style.color = colorStates[heart.style.color];
+        })
+        .catch(function (error) {
+          alert("Something went wrong!");
+        });
+    });
+  }
+});
 
 
 //------------------------------------------------------------------------------
