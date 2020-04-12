cca699c86367dd552a773d72c11f532de39dc77d
sydnee510 <sbsydnee161416@gmail.com>
2020-04-12 01:40:41 -0700

    Done.

--- main.js
+++ main.js
@@ -3,7 +3,40 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likes = document.querySelectorAll('.like');
+likes.forEach(el => {
+  el.addEventListener('click', heart => clickForLikes(heart))
+});
 
+function clickForLikes(heart){
+
+  const heartEl = heart.target.childNodes[1];
+  const isHeartEmpty = (heartEl.innerText === EMPTY_HEART) ? true : false;
+
+  if (isHeartEmpty) {
+    mimicServerCall()
+      .then(resp => {
+        // show like
+        heartEl.innerText = FULL_HEART;
+        heartEl.classList.add('activated-heart');
+      })
+      .catch(error => {
+        //  display the error message above!!
+        const modalEl = document.querySelector('#modal');
+        modalEl.textContent = error;
+        modalEl.classList.remove("hidden");
+
+        //  error message will be removed after 5 sec
+        setTimeout(error => {
+          modalEl.classList.add("hidden");
+        }, 5000)
+      })
+  } else {
+    //  remove the like when clicked
+    heartEl.classList.remove('activated-heart')
+    heartEl.innerText = EMPTY_HEART;
+  }
+}
 
 
 
